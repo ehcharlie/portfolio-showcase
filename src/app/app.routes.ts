f6b7c5069617a2608b35fcd1c03bf751f5ad824f
Charles Hook <charles.hook96@outlook.com>
2025-12-25 20:45:39 -0500

    updating project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import {Routes} from '@angular/router';
 import {AboutComponent} from './about/about.component';
-import {ResumeComponent} from './resume/resume.component';
 import {SkillsComponent} from './skills/skills.component';
 
 export const routes: Routes = [
-  {path: 'home', component: AboutComponent},
   {path: 'about', component: AboutComponent},
   {path: 'skills', component: SkillsComponent},
   {path: '', redirectTo: 'about', pathMatch: 'full'},
