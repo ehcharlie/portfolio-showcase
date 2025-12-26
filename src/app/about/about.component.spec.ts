4a3fbf686fd69a8536f950a91e18528643377a55
Charles Hook <charles.hook96@outlook.com>
2025-12-25 20:15:18 -0500

    updating project

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainContentComponent } from './main-content.component';
+import { AboutComponent } from './about.component';
 
-describe('MainContentComponent', () => {
-  let component: MainContentComponent;
-  let fixture: ComponentFixture<MainContentComponent>;
+describe('AboutComponent', () => {
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MainContentComponent]
+      imports: [AboutComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MainContentComponent);
+    fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {ResumeComponent} from './resume/resume.component';
+import {SkillsComponent} from './skills/skills.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: AboutComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'skills', component: SkillsComponent},
+  {path: '', redirectTo: 'about', pathMatch: 'full'},
+];
